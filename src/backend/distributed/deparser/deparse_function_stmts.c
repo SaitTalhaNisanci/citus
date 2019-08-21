43686cd1f01b7cb663801abac92fa5d7c45f35d0
Hanefi Onaldi <hanefi@citusdata.com>
2019-08-21 16:00:45 +0300

    Support more actions in ALTER FUNC deparsing
    I added support for the following actions in this commit:
    
        CALLED ON NULL INPUT | RETURNS NULL ON NULL INPUT | STRICT
        IMMUTABLE | STABLE | VOLATILE | [ NOT ] LEAKPROOF
        [ EXTERNAL ] SECURITY INVOKER | [ EXTERNAL ] SECURITY DEFINER

--- src/backend/distributed/deparser/deparse_function_stmts.c
+++ src/backend/distributed/deparser/deparse_function_stmts.c
@@ -13,6 +13,8 @@
 #include "distributed/deparser.h"
 #include "lib/stringinfo.h"
 #include "nodes/makefuncs.h"
+#include "nodes/nodes.h"
+#include "nodes/value.h"
 #include "parser/parse_type.h"
 
 /* forward declaration for deparse functions */
@@ -50,11 +52,53 @@ deparse_drop_function_stmt(DropStmt *stmt)
 static void
 appendAlterFunctionStmt(StringInfo buf, AlterFunctionStmt *stmt)
 {
+	ListCell *actionCell = NULL;
 	appendStringInfo(buf, "ALTER FUNCTION ");
 	appendFunctionName(buf, stmt->func);
 
-	/* TODO: use other attributes to deparse the query here  */
+	foreach(actionCell, stmt->actions)
+	{
+		DefElem *def = castNode(DefElem, lfirst(actionCell));
+		ereport(LOG, (errmsg("defElem found"),
+					  errdetail("name: %s", def->defname)));
 
+		if (strcmp(def->defname, "strict") == 0)
+		{
+			if (intVal(def->arg) == 1)
+			{
+				appendStringInfo(buf, " STRICT");
+			}
+			else
+			{
+				appendStringInfo(buf, " CALLED ON NULL INPUT");
+			}
+		}
+		else if (strcmp(def->defname, "volatility") == 0)
+		{
+			appendStringInfo(buf, " %s", strVal(def->arg));
+		}
+		else if (strcmp(def->defname, "leakproof") == 0)
+		{
+			if (intVal(def->arg) == 0)
+			{
+				appendStringInfo(buf, " NOT");
+			}
+			appendStringInfo(buf, " LEAKPROOF");
+		}
+		else if (strcmp(def->defname, "security") == 0)
+		{
+			if (intVal(def->arg) == 0)
+			{
+				appendStringInfo(buf, " SECURITY INVOKER");
+			}
+			else
+			{
+				appendStringInfo(buf, " SECURITY DEFINER");
+			}
+		}
+	}
+
+	/* TODO: use other attributes to deparse the query here  */
 	appendStringInfoString(buf, ";");
 }
 
